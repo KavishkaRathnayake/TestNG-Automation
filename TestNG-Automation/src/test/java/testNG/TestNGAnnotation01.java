274159a4972158f0d71a0f1bb54c5ad500a6241d
KavishkaRathnayake <medhangakavishka06@gmail.com>
2024-10-12 14:02:26 +0530

    1.1 - @Test Annotation

---
+++ TestNG-Automation/src/test/java/testNG/TestNGAnnotation01.java
@@ -0,0 +1,11 @@
+package testNG;
+
+import org.testng.annotations.Test;
+
+public class TestNGAnnotation01 {
+
+    @Test
+    public void test(){
+        System.out.println("Test 01");  //no need meain method to run a test
+    }
+}
