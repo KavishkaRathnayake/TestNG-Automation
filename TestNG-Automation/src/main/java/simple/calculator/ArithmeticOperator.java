8cce91b0aa0b47ee3734f7e729f451bad6361595
KavishkaRathnayake <medhangakavishka06@gmail.com>
2024-10-29 11:46:48 +0530

    1.7 - Arithmetic Operator

---
+++ TestNG-Automation/src/main/java/simple/calculator/ArithmeticOperator.java
@@ -0,0 +1,13 @@
+package simple.calculator;
+
+public class ArithmeticOperator {
+
+    public static int CalSum(int x , int y){
+        return x+y;
+    }
+
+    public static int CalSubstraction(int x , int y){
+        return x-y;
+    }
+
+}
